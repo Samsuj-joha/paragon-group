f2afa98907e0f11b8a046eae5dcfcf55406ff448
Samsujjoha <samsujjoha@paragon.com.bd>
2024-10-23 11:49:49 +0600

    added the about branch

---
+++ paragon/app/components/about/page.js
@@ -0,0 +1,12 @@
+
+
+const About = () => {
+  return (
+    <div>
+      <h1>this is the about page</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, omnis.</p>
+    </div>
+  )
+}
+
+export default About
